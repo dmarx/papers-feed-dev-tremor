743ae3a975672d856dbdc6e9c5688df951568e0e
David Marx <david.marx84@gmail.com>
2025-03-28 22:38:48 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
-  images: { 
-    unoptimized: true 
-  },
-  basePath: '/${{ github.event.repository.name }}',
-  assetPrefix: '/${{ github.event.repository.name }}/',
-  trailingSlash: true,
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
